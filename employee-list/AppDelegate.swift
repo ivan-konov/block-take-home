6e2c0489d0e82cc56b2472871dd5004f5be9b2d6
Ivan Konov <regs.of.ik@gmail.com>
2022-12-23 20:56:33 -0800

    Set image cache age to infinity

--- employee-list/AppDelegate.swift
+++ employee-list/AppDelegate.swift
@@ -11,16 +11,22 @@ import SDWebImage
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Set SDWebImage cahce sizes.
-        SDImageCacheConfig.default.maxDiskSize = 150000000
-        SDImageCacheConfig.default.maxMemoryCost = 50000000
+        self.setupImageCaching()
         
         return true
     }
     
-    // MARK: UISceneSession Lifecycle
+    // MARK: - UISceneSession Lifecycle
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    // MARK: - Image Caching Setup
+    
+    private func setupImageCaching() {
+        SDImageCacheConfig.default.maxDiskSize = 150000000
+        SDImageCacheConfig.default.maxMemoryCost = 50000000
+        SDImageCacheConfig.default.maxDiskAge = .infinity
     }
 }
